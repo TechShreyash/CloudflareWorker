886d6375e242019999f3b9576255b54f195b5372
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-02-05 22:20:21 +0530

    gogo anime domain changed

--- animedexapi/src/gogo.js
+++ animedexapi/src/gogo.js
@@ -4,7 +4,7 @@ import {
 } from "./gogo_extractor.js";
 import cheerio from "cheerio";
 
-const BaseURL = "https://gogoanime3.net";
+const BaseURL = "https://gogoanime3.co";
 const USER_AGENT =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
 
