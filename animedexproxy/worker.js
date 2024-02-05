f72ce053f91d84dd7af9df9b370ea39c3a4e3374
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-02-05 12:56:29 +0530

    Major Fixes
    Handling Cors request in animedexapi

--- animedexproxy/worker.js
+++ animedexproxy/worker.js
@@ -1,11 +1,11 @@
 async function handleRequest(request) {
     const url = new URL(request.url)
     let apiurl = url.searchParams.get('u')
-    apiurl = apiurl.replaceAll(' ', '+')
 
     if (apiurl === null) {
         return new Response('Cors Proxy Is Working\n\nUsage: https://proxy.techzbots1.workers.dev/?u=https://example.com')
     }
+    apiurl = apiurl.replaceAll(' ', '+')
 
     request = new Request(apiurl, request)
     request.headers.set('Origin', new URL(apiurl).origin)
@@ -16,21 +16,44 @@ async function handleRequest(request) {
     }
 
     let response = await fetch(request)
-    response = new Response(response.body, response)
 
+    response = new Response(response.body, response)
     response.headers.set('Access-Control-Allow-Origin', "*")
     response.headers.append('Vary', 'Origin')
     return response
 }
 
+function handleOptions(request) {
+    if (
+        request.headers.get('Origin') !== null &&
+        request.headers.get('Access-Control-Request-Method') !== null &&
+        request.headers.get('Access-Control-Request-Headers') !== null
+    ) {
+        return new Response(null, {
+            headers: corsHeaders,
+        })
+    } else {
+        return new Response(null, {
+            headers: {
+                Allow: 'GET, HEAD, POST, OPTIONS',
+            },
+        })
+    }
+}
+
+
 addEventListener('fetch', event => {
     const request = event.request
     const url = new URL(request.url)
-
-
-    if (
-        request.method === 'GET'
+    if (request.method === 'OPTIONS') {
+        // Handle CORS preflight requests
+        event.respondWith(handleOptions(request))
+    } else if (
+        request.method === 'GET' ||
+        request.method === 'HEAD' ||
+        request.method === 'POST'
     ) {
+        // Handle requests to the API server
         const response = handleRequest(request)
         event.respondWith(response)
     } else {
@@ -42,3 +65,9 @@ addEventListener('fetch', event => {
         })
     }
 })
+
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+}
